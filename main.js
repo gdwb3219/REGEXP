b55207e49ff4fa3ffde0e4270de26c9ec591e7cf
gdwb3219 <gdwb3219@gmail.com>
2023-02-19 21:43:41 +0900

    Create project

---
+++ main.js
@@ -0,0 +1,15 @@
+const str = `
+010-1234-5678
+thesecon@gmail.com
+https://www.omdb/com/?apikey=7035c60c&s=frozen
+The quick brown fox jumps over the lazy dog.
+abbcccdddd
+http://localhost:1234
+둥해물과_백두산이 마르고 닳도록
+`;
+
+const h = `  the hello  world    !
+
+`;
+// const regexp = /the/gi;
+console.log(h.replace(/\s/g, ''));
